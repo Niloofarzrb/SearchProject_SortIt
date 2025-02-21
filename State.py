5e2f86d11c7d248f70d67aecd5ece804c7fe20d5
niloofarzarabian <niloozrb80@gmail.com>
2025-02-21 19:41:34 +0330

    final

---
+++ State.py
@@ -0,0 +1,21 @@
+# this class only for the first time setup init state for problem and is given to every search
+class State:
+    def __init__(self, pipes: list, parent, g_n: int, prev_action: tuple):
+        self.pipes = pipes
+        self.parent = parent
+        self.g_n = g_n
+        self.prev_action = prev_action
+
+
+    def __hash__(self):
+        hash_strings = []
+        for i in self.pipes:
+            hash_strings.append(i.__hash__())
+        hash_strings = sorted(hash_strings)
+        hash_string = ''
+        for i in hash_strings:
+            hash_string += i + '###'
+        return hash_string
+
+    def __lt__(self, other):
+        return 0
